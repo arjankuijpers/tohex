c338e6e9a896b7ea7efb9e436a62f5a25693717b
Arjan Kuijpers <arjankuijpers@live.nl>
2018-04-18 15:46:03 +0200

    Application now writes to file instead of std out.

--- src/tohex.cpp
+++ src/tohex.cpp
@@ -1,12 +1,16 @@
 //---------------------------------------------------------------------------
 // ToHex
 // Convert binary file to C/C++ header file containing source file as a char array.
-// Written by Sergey Vasyutin (svpro [at] outlook.com).
+// Originally Written by Sergey Vasyutin (svpro [at] outlook.com).
+// Modified by Arjan Kuijpers 2018
 //---------------------------------------------------------------------------
 
 #include <stdio.h>
 #include <string>
 #include <algorithm>
+
+#include <iostream>
+#include <fstream>
 
 //---------------------------------------------------------------------------
 void Usage(void)
@@ -40,6 +44,15 @@ if(!fp) {
 	return 1;
 	}
 
+std::ofstream outputFile;
+outputFile.open(FileName + ".h", std::ios::binary & std::ios::out);
+if (!outputFile.is_open())
+{
+	fclose(fp);
+	return 2;
+}
+
+
 fseek(fp, 0, SEEK_END);
 Size = ftell(fp);
 fseek(fp, 0, SEEK_SET);
@@ -57,17 +70,32 @@ FileName.assign(Start, Last);
 //
 printf("/* This file was generated automatically. Do not edit. */\n"
    "#ifndef DATA_DECLARATION_ONLY\n\n");
+
+outputFile << "/* This file was generated automatically. Do not edit. */\n"
+"#ifndef DATA_DECLARATION_ONLY\n\n";
+
+outputFile << "unsigned char " << FileName.c_str() << "[" << Size << "] = {";
+
+
 printf("unsigned char %s[%ld] = {", FileName.c_str(), Size);
 while((c = fgetc(fp)) != EOF) {
    Count++;
    if(SymbolInStringCount == 16) SymbolInStringCount = 0;
-   if(!SymbolInStringCount) printf("\n\t");
-   printf("0x%02x%c ", c, (Count == Size)? ' ': ',');
+   if (!SymbolInStringCount) { printf("\n\t"); outputFile << "\n\t"; }
+   outputFile << "0x" << std::hex << c;
+   outputFile << ((Count == Size) ? ' ' : ',');
+   printf("0x%02x%c ", c, (Count == Size) ? ' ' : ',');
    SymbolInStringCount++;
    }
+
+outputFile << "\n\t};\n#else\n";
 printf("\n\t};\n#else\n");
+outputFile << "extern unsigned char " << FileName.c_str() << "[" << std::dec << Size << "];\n";
 printf("extern unsigned char %s[%ld];\n", FileName.c_str(), Size);
 printf("#endif\n\n/* EOF */\n");
+
+outputFile << "#endif\n\n/* EOF */\n";
+outputFile.close();
 
 return 0;
 }
